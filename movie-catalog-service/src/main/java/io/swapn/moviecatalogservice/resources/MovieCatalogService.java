dc0ff55ada276cc673c93161f694c2aaeab3660a
swapn <swapnilshiketod@gmail.com>
2021-05-18 03:46:24 +0530

    04:Using RestTemplate

--- movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/resources/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/resources/MovieCatalogService.java
@@ -1,22 +1,39 @@
 package io.swapn.moviecatalogservice.resources;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import io.swapn.moviecatalogservice.models.CatalogItem;
+import io.swapn.moviecatalogservice.models.Movie;
+import io.swapn.moviecatalogservice.models.Rating;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogService {
 
+	@Autowired
+	RestTemplate template;
+	
 	@GetMapping("/{userId}")
-	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-		return Collections.singletonList(
-					new CatalogItem("Transformers", "Test", 4));
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+		//RestTemplate template = new RestTemplate();
+		//Movie movie = template.getForObject("http://localhost:8081/movies/foo", Movie.class);
+		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
+
+		return ratings.stream()
+					  .map(rating -> {
+						Movie movie = template.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);					  
+						  return new CatalogItem(movie.getName(), "Desc", rating.getRating());
+						  })
+					  .collect(Collectors.toList());
+
 	}
 }
