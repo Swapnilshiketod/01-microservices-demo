a244fcd51a91f6bb2e908dd8fdbaf69044d769de
swapn <swapnilshiketod@gmail.com>
2021-05-18 23:41:23 +0530

    WebClient in Action

--- movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/resources/MovieCatalogService.java
+++ movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/resources/MovieCatalogService.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.swapn.moviecatalogservice.models.CatalogItem;
 import io.swapn.moviecatalogservice.models.Movie;
@@ -20,17 +21,30 @@ import io.swapn.moviecatalogservice.models.Rating;
 public class MovieCatalogService {
 
 	@Autowired
-	RestTemplate template;
+	RestTemplate template;	
+	
+	@Autowired
+	public WebClient.Builder builder;
 	
 	@GetMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+		
+		
+		
 		//RestTemplate template = new RestTemplate();
 		//Movie movie = template.getForObject("http://localhost:8081/movies/foo", Movie.class);
 		List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("5678", 3));
 
 		return ratings.stream()
 					  .map(rating -> {
-						Movie movie = template.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);					  
+						//Movie movie = template.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);					  
+						  
+    Movie movie =  builder.build()
+						  .get()
+						  .uri("http://localhost:8081/movies/" + rating.getMovieId())
+						  .retrieve()
+						  .bodyToMono(Movie.class)
+						  .block();
 						  return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 						  })
 					  .collect(Collectors.toList());
