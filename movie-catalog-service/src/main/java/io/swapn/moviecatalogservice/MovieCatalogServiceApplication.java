24cba96dee2101ea1b930b4ea0f92b403b009ec1
swapn <swapnilshiketod@gmail.com>
2021-05-22 19:52:03 +0530

    committing from br01

--- movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/swapn/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -19,10 +19,10 @@ public class MovieCatalogServiceApplication {
 		return WebClient.builder();	
 	}
 	
-	@Bean
-	public WebClient.Builder getWebClientBuilder(){
-		return WebClient.builder();	
-	}
+	/*
+	 * @Bean public WebClient.Builder getWebClientBuilder(){ return
+	 * WebClient.builder(); }
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
 	}
